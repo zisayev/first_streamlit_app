8f95703c4ed5b72bec4c00e1c8e574b1342ef9ea
zisayev <98119705+zisayev@users.noreply.github.com>
2023-03-04 23:22:48 -0500

    Update streamlit_app.py
    breakfast menu entered

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title(
+'My Parents New Healthy Diner
+
+Breakfast Menu
+
+Omega 3 & Bluberry Oatmeal
+
+Kale, Spinach & Rocket Smoothie
+
+Hard-Boiled Free-Range Egg')
