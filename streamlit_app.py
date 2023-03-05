7d6650cf19086dbbb57cc67932337570ad564b7a
zisayev <98119705+zisayev@users.noreply.github.com>
2023-03-05 08:49:14 -0500

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
