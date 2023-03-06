f8de29fcaa873a13000fb5b4871d96aa466ce20a
zisayev <98119705+zisayev@users.noreply.github.com>
2023-03-06 12:35:35 -0500

    Update streamlit_app.py
    new commit removal

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# streamlit.text(fruityvice_response.json()) => removing it as per exercise instructions
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
